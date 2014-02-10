29025873fa24b61f7452e23cbdd63264a437d740
faassen <faassen@startifact.com>
2014-02-10 17:39:49 +0800

    A test for explicitly combining variables.

--- morepath/tests/test_subpath_directive.py
+++ morepath/tests/test_subpath_directive.py
@@ -7,7 +7,7 @@ from werkzeug.test import Client
 import pytest
 
 
-def test_subpath():
+def test_subpath_implicit_variables():
     config = setup()
     app = morepath.App(testing_config=config)
 
@@ -32,6 +32,55 @@ def test_subpath():
     @app.view(model=Item)
     def default(self, request):
         return "Item %s for parent %s" % (self.id, self.parent.container_id)
+
+    @app.view(model=Item, name='link')
+    def link(self, request):
+        return request.link(self)
+
+    config.commit()
+
+    c = Client(app, Response)
+
+    response = c.get('/A/a')
+    assert response.data == 'Item a for parent A'
+
+    response = c.get('/B/a')
+    assert response.data == 'Item a for parent B'
+
+    response = c.get('/A/a/link')
+    assert response.data == '/A/a'
+
+    response = c.get('/B/a/link')
+    assert response.data == '/B/a'
+
+
+def test_subpath_explicit_variables():
+    config = setup()
+    app = morepath.App(testing_config=config)
+
+    class Container(object):
+        def __init__(self, id):
+            self.id = id
+
+    class Item(object):
+        def __init__(self, parent, id):
+            self.parent = parent
+            self.id = id
+
+    @app.path(model=Container, path='{container_id}',
+              variables=lambda m: dict(container_id=m.id))
+    def get_container(container_id):
+        return Container(container_id)
+
+    @app.subpath(model=Item, path='{id}', base=Container,
+                 get_base=lambda m: m.parent,
+                 variables=lambda m: dict(id=m.id))
+    def get_item(base, id):
+        return Item(base, id)
+
+    @app.view(model=Item)
+    def default(self, request):
+        return "Item %s for parent %s" % (self.id, self.parent.id)
 
     @app.view(model=Item, name='link')
     def link(self, request):
